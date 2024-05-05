5a77d85a0fce2b14bd28ee543fa61c0001923cba
Habchi-Abdennour <164701825+Habchi-Abdennour@users.noreply.github.com>
2024-05-05 23:13:31 +0200

    Add files via upload

---
+++ class_arima.py
@@ -0,0 +1,50 @@
+import numpy as np
+from pmdarima import auto_arima
+from statsmodels.tsa.arima.model import ARIMA
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+import warnings
+
+
+class TimeSeriesAnalysis:
+    def __init__(self, dates, ordered):
+        self.dates = pd.to_datetime(dates)
+        self.ordered = np.array(ordered)
+        self.df = pd.DataFrame({'date': self.dates, 'ordered': self.ordered})
+        self.df['date'] = pd.to_datetime(self.df['date'])
+        self.df.set_index('date', inplace=True)
+
+    def check_stationarity(self, column_name):
+        result = adfuller(self.df[column_name], autolag='AIC')
+        return 1 if result[1] < 0.05 else 0
+
+    def preprocess_data(self):
+        while self.check_stationarity('ordered') == 0:
+            self.df['ordered'] = self.df['ordered'] - self.df['ordered'].shift(1)
+            self.df['ordered'] = self.df['ordered'].fillna(self.df['ordered'].mean())
+
+    def fit_arima_model(self, train_size=0.7):
+        train_index = int(len(self.df) * train_size)
+        train_data = self.df['ordered'][:train_index]
+
+        model = auto_arima(y=train_data)
+        order_par = model.order
+        p, d, q = order_par
+        model = ARIMA(train_data, order=(p, d, q))
+        model_fit = model.fit()
+        return model_fit
+
+    def make_predictions(self, steps):
+        model_fit = self.fit_arima_model()
+        prediction = model_fit.forecast(steps=steps)
+        return prediction
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from typing import Optional
 
 from fastapi import FastAPI
+import requests,uvicorn
+from class_arima import TimeSeriesAnalysis
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def main():
+    dates = ['2022-01-01', '2022-02-01', '2022-03-01', '2022-04-01',
+         '2022-05-01', '2022-06-01', '2022-07-01', '2022-08-01',
+         '2022-09-01', '2022-10-01', '2022-11-01', '2022-12-01',
+         '2023-01-01', '2023-02-01', '2023-03-01', '2023-04-01',
+         '2023-05-01', '2023-06-01', '2023-07-01', '2023-08-01',
+         '2023-09-01', '2023-10-01', '2023-11-01', '2023-12-01']
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    ordered = [45, 22, 80, 30, 70, 100, 90, 115, 150, 120, 200, 175,
+           200, 250, 100, 120, 30, 50, 100, 75, 250, 150, 205, 175]
+    return {'dates':dates,'ordered':ordered}
+
+
+
+
+@app.get("/predict")
+def read_root(url:str,value:int):
+    read=requests.get(url)
+    format=read.json()
+    dates=format['dates']
+    ordered=format['ordered']
+    tsa = TimeSeriesAnalysis(dates, ordered)
+    tsa.preprocess_data()
+    predictions = tsa.make_predictions(value)
+
+    return  {'dates': predictions.index.strftime('%Y-%m-%d').tolist(), 'ordered': predictions.tolist()}
